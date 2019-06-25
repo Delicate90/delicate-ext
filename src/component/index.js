90ade287afd55c5cb3d307e86c01038a4d3fa126
cuixiang <cuixiang@feitaike.com.cn>
2019-06-25 18:55:15 +0800

    add Component.Async

--- src/component/index.js
+++ src/component/index.js
@@ -1,9 +1,11 @@
 import _FormInModal from './FormInModal'
 import _Modal from './Modal'
 import _Drawer from './Drawer'
+import _Async from './Async'
 
 export default {
     FormInModal: _FormInModal,
     Modal: _Modal,
-    Drawer: _Drawer
+    Drawer: _Drawer,
+    Async: _Async
 }
